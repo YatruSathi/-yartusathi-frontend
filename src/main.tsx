2a1cc7cfb56cf4e55b4768d8f2eefab40fa53e5b
Kriti Upadhyay <kritiu54@gmail.com>
2025-07-12 22:48:16 +0545

    setup material ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {  RouterProvider, } from "react-router";
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import theme from './theme';
 import routes from './routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+     <ThemeProvider theme={theme}>
+      <CssBaseline />
     <RouterProvider router={routes}/>
+    </ThemeProvider>
   </StrictMode>,
 )
 
